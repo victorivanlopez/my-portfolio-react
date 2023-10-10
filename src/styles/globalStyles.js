2d0142bdc0c2be42cdf0912109de3bd8bb2db785
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-10 16:10:22 -0600

    Added: Global Styles

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,88 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+    scroll-behavior: smooth;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: ${({ theme }) => theme.fonts.poppins[0] || theme.fonts.poppins[1]};
+    font-size: 1.6rem;
+    line-height: 1.5;
+  }
+
+  h1,
+  h2,
+  h3 {
+    font-family: ${({ theme }) => theme.fonts.lato[0] || theme.fonts.lato[1]};
+    margin: 0 0 1.5rem 0;
+    letter-spacing: .2rem;
+    text-transform: uppercase;
+  }
+
+  h1 {
+    font-size: 3.5rem;
+    font-weight: 900;
+  }
+
+  h2 {
+    font-size: 2.8rem;
+    text-align: center;
+  }
+
+  h3 {
+    font-size: 2rem;
+  }
+
+  p {
+    margin: 0;
+    letter-spacing: .1rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
+
+  hr {
+    border: 1px solid ${({ theme }) => theme.colors.grayLight};
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+
+  /* Utils */
+  .container {
+    width: min(90%, 105rem);
+    margin: 0 auto;
+  }
+
+  .section {
+    padding: 5rem 0;
+  }
+
+  .section-description {
+    text-align: center;
+    margin-bottom: 5rem;
+  }
+`;
