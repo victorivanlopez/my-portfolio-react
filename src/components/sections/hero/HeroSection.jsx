d9ea889a5195a3dc04a586ed52f9e244849f2a2d
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-10 16:20:47 -0600

    Updated: button styles

--- src/components/sections/hero/HeroSection.jsx
+++ src/components/sections/hero/HeroSection.jsx
@@ -1,4 +1,4 @@
-import { ButtonPrimaryHero, Hero, HeroContent } from './styles';
+import { ButtonPrimary, Hero, HeroContent } from './styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
@@ -9,7 +9,7 @@ export const HeroSection = () => {
         <p>Hola. Mi nombre es Víctor Iván López</p>
         <h1>Desarrollador Web <span>Front-end</span></h1>
 
-        <ButtonPrimaryHero href="#">Ver Proyectos</ButtonPrimaryHero>
+        <ButtonPrimary href="#">Ver Proyectos</ButtonPrimary>
       </HeroContent>
 
       <a href='#'>
