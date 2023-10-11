19f056dbb449e0abe69237ccf1e07a0e686adf5e
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-11 13:24:19 -0600

    Created: barrel file

--- src/components/sections/index.js
+++ src/components/sections/index.js
@@ -1,4 +1,5 @@
 export * from './about/AboutSection';
+export * from './contact/ContactSection';
 export * from './hero/HeroSection';
 export * from './projects/ProjectsSection';
 export * from './resume/ResumeSection';
