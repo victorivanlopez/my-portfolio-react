d866f58edc592e763488986a6a2a569b64f0800e
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-14 13:28:35 -0600

    Added: ProjectItem component

--- src/components/sections/projects/ProjectsSection.jsx
+++ src/components/sections/projects/ProjectsSection.jsx
@@ -1,12 +1,5 @@
-import { 
-  ProjectContent, 
-  ProjectFooter, 
-  ProjectImage, 
-  ProjectItem, 
-  ProjectLinks, 
-  ProjectTechnologies, 
-  ProjectsList 
-} from './styles';
+import { ProjectsList } from './styles'; 
+import { ProjectItem } from './project';
 
 export const ProjectsSection = () => {
   return (
@@ -15,96 +8,7 @@ export const ProjectsSection = () => {
       <p className="section-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
       <ProjectsList>
-        <ProjectItem>
-          <ProjectImage>
-            <img className='img-project' src="/assets/projects/project2.png" alt="Imagen Proyecto" />
-          </ProjectImage>
-
-          <ProjectContent>
-            <h3>GuitarLA</h3>
-            <ProjectLinks>
-              <a href="#">Ver demo</a>
-              <a href="#">Ver Repositorio</a>
-            </ProjectLinks>
-          </ProjectContent>
-
-          <ProjectFooter>
-            <ProjectTechnologies>
-              <li>
-                <img src="/assets/projects/icon-nodejs.webp" alt="Icono Tecnología" />
-              </li>
-              <li>
-                <img src="/assets/projects/icon-express.webp" alt="Icono Tecnología" />
-              </li>
-              <li>
-                <img src="/assets/projects/icon-react.webp" alt="Icono Tecnología" />
-              </li>
-              <li>
-                <img src="/assets/projects/icon-mongodb.webp" alt="Icono Tecnología" />
-              </li>
-            </ProjectTechnologies>
-          </ProjectFooter>
-        </ProjectItem>
-        <ProjectItem>
-          <ProjectImage>
-            <img className='img-project' src="/assets/projects/project3.png" alt="Imagen Proyecto" />
-          </ProjectImage>
-
-          <ProjectContent>
-            <h3>Podcast App</h3>
-            <ProjectLinks>
-              <a href="#">Ver demo</a>
-              <a href="#">Ver Repositorio</a>
-            </ProjectLinks>
-          </ProjectContent>
-
-          <ProjectFooter>
-            <ProjectTechnologies>
-              <li>
-                <img src="/assets/projects/icon-sass.webp" alt="Icono Tecnología" />
-              </li>
-              <li>
-                <img src="/assets/projects/icon-js.webp" alt="Icono Tecnología" />
-              </li>
-              <li>
-                <img src="/assets/projects/icon-gulp.webp" alt="Icono Tecnología" />
-              </li>
-              <li>
-                <img src="/assets/projects/icon-nodejs.webp" alt="Icono Tecnología" />
-              </li>
-            </ProjectTechnologies>
-          </ProjectFooter>
-        </ProjectItem>
-        <ProjectItem>
-          <ProjectImage>
-            <img className='img-project' src="/assets/projects/project1.png" alt="Imagen Proyecto" />
-          </ProjectImage>
-
-          <ProjectContent>
-            <h3>Air Viaje</h3>
-            <ProjectLinks>
-              <a href="#">Ver demo</a>
-              <a href="#">Ver Repositorio</a>
-            </ProjectLinks>
-          </ProjectContent>
-
-          <ProjectFooter>
-            <ProjectTechnologies>
-              <li>
-                <img src="/assets/projects/icon-nodejs.webp" alt="Icono Tecnología" />
-              </li>
-              <li>
-                <img src="/assets/projects/icon-express.webp" alt="Icono Tecnología" />
-              </li>
-              <li>
-                <img src="/assets/projects/icon-react.webp" alt="Icono Tecnología" />
-              </li>
-              <li>
-                <img src="/assets/projects/icon-mongodb.webp" alt="Icono Tecnología" />
-              </li>
-            </ProjectTechnologies>
-          </ProjectFooter>
-        </ProjectItem>
+        <ProjectItem />
       </ProjectsList>
     </main>
   )
