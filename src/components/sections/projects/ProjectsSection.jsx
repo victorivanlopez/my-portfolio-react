aafb12f8c7e4cb886d25f48501f01a22fed1b4ea
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-15 19:32:05 -0600

    Added: useFetch implementation

--- src/components/sections/projects/ProjectsSection.jsx
+++ src/components/sections/projects/ProjectsSection.jsx
@@ -1,14 +1,24 @@
-import { ProjectsList } from './styles'; 
+import { ProjectsList } from './styles';
 import { ProjectItem } from './project';
+import { useFetch } from '../../../hooks';
 
 export const ProjectsSection = () => {
+
+  const { data: projects, isLoading } = useFetch('http://127.0.0.1:1337/api/projects?populate=*');
+
   return (
     <main id='projects' className="section container observer">
       <h2>Proyectos</h2>
       <p className="section-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
       <ProjectsList>
-        <ProjectItem />
+        {
+          (isLoading)
+            ? <p>Loading...</p>
+            : projects?.map(project => (
+              <ProjectItem key={project.id} project={project.attributes} />
+            ))
+        }
       </ProjectsList>
     </main>
   )
