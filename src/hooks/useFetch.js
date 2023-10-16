b795fa5aa791a074ce8473a3796d05ca11a497b8
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-15 19:28:33 -0600

    Created: useFetch custom hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+
+export const useFetch = (url) => {
+
+  const [state, setState] = useState({
+    data: null,
+    isLoading: true,
+    hasError: null,
+  });
+
+  const getFetch = async () => {
+    setState({
+      ...state,
+      isLoading: true
+    });
+
+    try {
+      const resp = await fetch(url);
+      const { data } = await resp.json();
+      setState({
+        data,
+        isLoading: false,
+        hasError: null,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        hasError: error
+      });
+    }
+  };
+
+  useEffect(() => {
+    getFetch();
+  }, [url]);
+
+  return {
+    ...state
+  };
+}
