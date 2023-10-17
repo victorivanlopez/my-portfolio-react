5aa3a9d0a2f4a745d18057d6a243f60a2d86e0a4
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-16 19:22:06 -0600

    Changed: folders

--- src/components/sections/projects/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const ProjectsList = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(min(100%, 30rem), 1fr));
-  gap: 4rem;
-  
-`;
