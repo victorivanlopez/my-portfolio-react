7b1a7f62df7024c429d6726edf5074724ae27614
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-16 21:22:51 -0600

    Added: conditional for empty projects

--- src/components/sections/projects/projectsList/ProjectsList.jsx
+++ src/components/sections/projects/projectsList/ProjectsList.jsx
@@ -1,13 +1,15 @@
-import { Projects } from './styles';
 import { ProjectItem } from '../projectItem';
+import { Projects } from './styles';
 
 export const ProjectsList = ({ projects }) => {
   return (
     <Projects>
       {
-        projects?.map(project => (
-          <ProjectItem key={project.id} project={project.attributes} />
-        ))
+        (projects.length < 1)
+          ? <p>Aún no hay proyectos.</p>
+          : projects?.map(project => (
+            <ProjectItem key={project.id} project={project.attributes} />
+          ))
       }
     </Projects>
   )
