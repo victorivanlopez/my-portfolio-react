05b2351082f35a5b704f520c7718a4726817b546
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-16 19:24:21 -0600

    Added: errors management

--- src/components/sections/projects/ProjectsSection.jsx
+++ src/components/sections/projects/ProjectsSection.jsx
@@ -1,25 +1,23 @@
-import { ProjectsList } from './styles';
-import { ProjectItem } from './project';
 import { useFetch } from '../../../hooks';
+import { Spinner } from '../../../ui/components';
+import { ProjectsList } from './projectsList';
 
 export const ProjectsSection = () => {
 
-  const { data: projects, isLoading } = useFetch('http://127.0.0.1:1337/api/projects?populate=*');
+  const { data: projects, isLoading, hasError } = useFetch('http://127.0.0.1:1337/api/projects?populate=*');
 
   return (
     <main id='projects' className="section container observer">
       <h2>Proyectos</h2>
       <p className="section-description">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
-      <ProjectsList>
-        {
-          (isLoading)
-            ? <p>Loading...</p>
-            : projects?.map(project => (
-              <ProjectItem key={project.id} project={project.attributes} />
-            ))
-        }
-      </ProjectsList>
+      {(hasError) && <p>{hasError}</p>}
+
+      {
+        (isLoading)
+          ? <Spinner />
+          : <ProjectsList projects={projects} />
+      }
     </main>
   )
 }
