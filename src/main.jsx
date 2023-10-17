4657ff31e028b57c6052ac333599dd2db71a13f5
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-16 19:22:38 -0600

    Added: css spinner component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { PortfolioApp } from './PortfolioApp';
 import './styles/normalize.css';
 import { AppTheme } from './theme/AppTheme';
+import './styles/spinner.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
