d63883a539c92bbbc5e237793b9c262e0eb553e0
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-17 18:37:28 -0600

    Updated: styles

--- src/ui/components/alert/styles.js
+++ src/ui/components/alert/styles.js
@@ -6,6 +6,7 @@ export const AlertContainer = styled.div`
   gap: 2rem;
   padding: 2rem;
   background-color: ${({ type }) => type === 'error' ? '#FFF0F0' : '#C4F9E2'};
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 4px 0px;
   border-radius: 1rem;
 
   p, svg {
