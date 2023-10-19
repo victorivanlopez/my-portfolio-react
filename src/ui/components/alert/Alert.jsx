f531f069aa13095b450be2ac143838bd3851db4c
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-19 16:52:44 -0600

    Added: dynamic icon

--- src/ui/components/alert/Alert.jsx
+++ src/ui/components/alert/Alert.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { AlertContainer } from './styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck, faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
 
 export const Alert = ({ message, type = 'success' }) => {
   return (
     <AlertContainer type={type}>
-      <FontAwesomeIcon icon={faCircleExclamation} />
+      {
+        type === 'success'
+          ? <FontAwesomeIcon icon={faCircleCheck} />
+          : <FontAwesomeIcon icon={faCircleExclamation} />
+      }
       <p>{message}</p>
     </AlertContainer>
   )
