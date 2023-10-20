35ccf5c15530b5bbb6f4697eb609e65031d5825b
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-19 22:09:28 -0600

    Added: animation class

--- src/components/sections/resume/experiences/ExperiencesList.jsx
+++ src/components/sections/resume/experiences/ExperiencesList.jsx
@@ -3,14 +3,14 @@ import { ResumeExperience } from './styles';
 export const ExperiencesList = () => {
   return (
     <>
-      <ResumeExperience>
+      <ResumeExperience className='scale-up-top'>
         <p className="dates">2021 - 2023</p>
         <h3>Freelancer</h3>
         <p className="company">victorivanlopez.com</p>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis autem minima velit quibusdam vero.</p>
       </ResumeExperience>
 
-      <ResumeExperience>
+      <ResumeExperience className='scale-up-top'>
         <p className="dates">2018 - 2020</p>
         <h3>Coordinador de Informática</h3>
         <p className="company">ASIPONA Manzanillo</p>
--- src/components/sections/resume/skills/SkillsList.jsx
+++ src/components/sections/resume/skills/SkillsList.jsx
@@ -1,17 +1,26 @@
 import { ResumeSkill } from './styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFigma, faReact } from '@fortawesome/free-brands-svg-icons';
+import { faCodeBranch, faDatabase } from '@fortawesome/free-solid-svg-icons';
 
 export const SkillsList = () => {
   return (
     <>
-      <ResumeSkill>
-        <FontAwesomeIcon icon={faFigma} size="2xl" />
+      <ResumeSkill className='scale-up-top'>
+        <FontAwesomeIcon icon={faFigma} />
         <h3>Maquetación y Diseño</h3>
       </ResumeSkill>
-      <ResumeSkill>
+      <ResumeSkill className='scale-up-top'>
         <FontAwesomeIcon icon={faReact} />
         <h3>Desarrollo con React</h3>
+      </ResumeSkill>
+      <ResumeSkill className='scale-up-top'>
+        <FontAwesomeIcon icon={faDatabase} />
+        <h3>Manejo de bases de datos</h3>
+      </ResumeSkill>
+      <ResumeSkill className='scale-up-top'>
+        <FontAwesomeIcon icon={faCodeBranch} />
+        <h3>Uso de Git y GitHub</h3>
       </ResumeSkill>
     </>
   )
