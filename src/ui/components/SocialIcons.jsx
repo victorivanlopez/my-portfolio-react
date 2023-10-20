cd2b8d81a768d458b251a950e79595512a9b92b3
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-19 22:09:13 -0600

    Added: rel attribute

--- src/ui/components/SocialIcons.jsx
+++ src/ui/components/SocialIcons.jsx
@@ -4,10 +4,10 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 export const SocialIcons = () => {
   return (
     <>
-      <a href="https://mx.linkedin.com/" target="_blank">
+      <a href="https://mx.linkedin.com/" target="_blank" rel='noreferrer'>
         <FontAwesomeIcon icon={faLinkedin} size='xl' />
       </a>
-      <a href="https://github.com/victorivanlopez" target="_blank">
+      <a href="https://github.com/victorivanlopez" target="_blank" rel='noreferrer'>
         <FontAwesomeIcon icon={faGithub} size='xl' />
       </a>
     </>
