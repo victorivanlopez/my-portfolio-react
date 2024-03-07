956e5a1bc0bd18432a8520870db0653b86597d4d
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-06 18:05:53 -0600

    Updated: styles grid

--- src/components/sections/resume/styles.js
+++ src/components/sections/resume/styles.js
@@ -29,11 +29,8 @@ export const TabsButtons = styled.button`
 export const ResumeContainer = styled.div`
   margin-top: 2rem;
   display: grid;
-  grid-template-columns: repeat(1, 1fr);
-  gap: 3rem;
-  @media (min-width: 768px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
+  grid-template-columns: repeat(2, 1fr);
+  gap: 2rem;
   @media (min-width: 1024px) {
     grid-template-columns: repeat(4, 1fr);
     ${(props) => (props.$current === 'experience') && css`
