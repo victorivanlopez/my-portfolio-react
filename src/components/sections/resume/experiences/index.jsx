e81a2dc7964e3c33bb93f028fa6c8dfe96383504
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-09 23:24:44 -0600

    Added: experiences functionality

--- src/components/sections/resume/experiences/index.jsx
+++ src/components/sections/resume/experiences/index.jsx
@@ -1,21 +1,17 @@
-import { ResumeExperience } from './styles';
+import { usePortfolioContext } from '../../../../hooks';
+import { Alert, Spinner } from '../../../../ui/components';
+import { ExperiencesList } from './experiences-list';
 
-export const ExperiencesList = () => {
+export const Experiences = () => {
+
+  const { experiences, isLoading, hasError } = usePortfolioContext();
+
   return (
     <>
-      <ResumeExperience className='scale-up-top'>
-        <p className="dates">2021 - 2023</p>
-        <h3>Freelancer</h3>
-        <p className="company">victorivanlopez.com</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis autem minima velit quibusdam vero.</p>
-      </ResumeExperience>
+      {(isLoading) && <Spinner />}
 
-      <ResumeExperience className='scale-up-top'>
-        <p className="dates">2018 - 2020</p>
-        <h3>Coordinador de Informática</h3>
-        <p className="company">ASIPONA Manzanillo</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis autem minima velit quibusdam vero.</p>
-      </ResumeExperience>
+      {(experiences) && <ExperiencesList experiences={experiences} />}
+      {(hasError) && <Alert message={hasError} type='error' />}
     </>
   )
 }
