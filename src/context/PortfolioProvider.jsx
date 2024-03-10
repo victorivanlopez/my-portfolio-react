c49d78c2185c1193205d9d417a3dcff2c4b5c920
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-03-09 23:13:08 -0600

    Added: experiences fetch

--- src/context/PortfolioProvider.jsx
+++ src/context/PortfolioProvider.jsx
@@ -3,43 +3,52 @@ import { PortfolioContext } from './';
 
 const urlProjects = `${import.meta.env.VITE_API_URL}/projects?populate=*`;
 const urlSkills = `${import.meta.env.VITE_API_URL}/skills?populate=*`;
+const urlExperiences = `${import.meta.env.VITE_API_URL}/experiences`;
 
 export const PortfolioProvider = ({ children }) => {
 
   const [projects, setProjects] = useState(null);
   const [skills, setSkills] = useState(null);
+  const [experiences, setExperiences] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [hasError, setHasError] = useState(null);
 
   const startFetchingData = async () => {
     setProjects(null);
     setSkills(null);
+    setExperiences(null);
     setIsLoading(true);
     setHasError(null);
     try {
-      const [resProjects, resSkills] = await Promise.all([
+      const [resProjects, resSkills, resExperiences] = await Promise.all([
         fetch(urlProjects),
         fetch(urlSkills),
+        fetch(urlExperiences),
       ]);
-      if (resProjects.status === 404 || resSkills.status === 404) {
+      if (resProjects.status === 404 || resSkills.status === 404 || resExperiences.status === 404) {
         setProjects(null);
         setSkills(null);
+        setExperiences(null);
         setIsLoading(false);
-        setHasError(`Error ${resp.status}: Servidor no encontrado.`);
+        setHasError(`Servidor no encontrado.`);
+        return;
       }
-      if (resProjects.status === 200 || resSkills.status === 200) {
-        const [{ data: dataProjects }, { data: dataSkills }] = await Promise.all([
+      if (resProjects.status === 200 || resSkills.status === 200 || resExperiences.status === 200) {
+        const [{ data: dataProjects }, { data: dataSkills }, { data: dataExperiences }] = await Promise.all([
           resProjects.json(),
           resSkills.json(),
+          resExperiences.json(),
         ]);
         setProjects(dataProjects);
         setSkills(dataSkills);
+        setExperiences(dataExperiences);
         setIsLoading(false);
         setHasError(null);
       }
     } catch (error) {
       setProjects(null);
       setSkills(null);
+      setExperiences(null);
       setIsLoading(false);
       setHasError('Error en el servidor.');
     }
@@ -56,6 +65,7 @@ export const PortfolioProvider = ({ children }) => {
         skills,
         isLoading,
         hasError,
+        experiences,
       }}
     >
       {children}
