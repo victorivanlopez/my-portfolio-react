245e7161f23d07110c76313bcf2d184a9d740f3a
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-04-11 21:01:10 -0600

    Updated: startFetchingData manage errors

--- src/context/PortfolioProvider.jsx
+++ src/context/PortfolioProvider.jsx
@@ -25,17 +25,34 @@ export const PortfolioProvider = ({ children }) => {
         fetch(urlSkills),
         fetch(urlExperiences),
       ]);
-      const [{ data: dataProjects }, { data: dataSkills }, { data: dataExperiences }] = await Promise.all([
-        resProjects.json(),
-        resSkills.json(),
-        resExperiences.json(),
-      ]);
-      setProjects(dataProjects);
-      setSkills(dataSkills);
-      setExperiences(dataExperiences);
-      setIsLoading(false);
-      setHasError(null);
-
+      if (resProjects.status === 404 || resSkills.status === 404 || resExperiences.status === 404) {
+        setProjects(null);
+        setSkills(null);
+        setExperiences(null);
+        setIsLoading(false);
+        setHasError(`Servidor no encontrado.`);
+        return;
+      }
+      if (resProjects.status === 401 || resSkills.status === 401 || resExperiences.status === 401) {
+        setProjects(null);
+        setSkills(null);
+        setExperiences(null);
+        setIsLoading(false);
+        setHasError(`Ha ocurrido un error en el servidor.`);
+        return;
+      }
+      if (resProjects.status === 200 || resSkills.status === 200 || resExperiences.status === 200) {
+        const [{ data: dataProjects }, { data: dataSkills }, { data: dataExperiences }] = await Promise.all([
+          resProjects.json(),
+          resSkills.json(),
+          resExperiences.json(),
+        ]);
+        setProjects(dataProjects);
+        setSkills(dataSkills);
+        setExperiences(dataExperiences);
+        setIsLoading(false);
+        setHasError(null);
+      }
     } catch (error) {
       setProjects(null);
       setSkills(null);
