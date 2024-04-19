b09915b4ed6d3cc921ccaac09953c1f7a2aa3271
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-04-18 22:26:25 -0600

    Created: styles

---
+++ src/layout/portfolio/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const MainContent = styled.div`
+  @media (min-width: 993px) {
+    margin-left: 20rem;
+  }
+`;
+
+export const MainContainer = styled.div`
+  overflow: auto;
+  height: 100vh;
+`;
