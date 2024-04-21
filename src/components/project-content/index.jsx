d4ad73316092075034c9c6318cbf402c105abbb4
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-04-20 22:33:19 -0600

    Added: dynamic functionality

--- src/components/project-content/index.jsx
+++ src/components/project-content/index.jsx
@@ -1,3 +1,5 @@
+import { BlocksRenderer } from '@strapi/blocks-react-renderer';
+import { usePortfolioContext } from '../../hooks';
 import {
   ContainerProject,
   ContentProject,
@@ -6,44 +8,44 @@ import {
   GridProject,
   LinksProject
 } from './styles';
+import { Spinner } from '../../ui/components';
 
 export const ProjectContent = () => {
+  const { project, isLoading } = usePortfolioContext();
+
+  if ((isLoading)) return <Spinner />
+
+  const { title, description, client, repoURL, siteURL, cover } = project?.attributes;
+
+  const imageURL = cover.data.attributes.formats.small.url;
+
   return (
     <ContainerProject>
-      <h2>TaskUnity</h2>
+      <h2>{title}</h2>
 
       <GridProject>
-        <img src="/assets/projects/taskunity-cover.png" alt="TaskUnity Cover" />
+        <img src={imageURL} alt={`Portada sitio web ${title}`} />
 
         <ContentProject>
           <h3>Descripción</h3>
           <DescriptionProject>
-            <ul>
-              <li>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, impedit eos repudiandae rerum quis nam quos! Incidunt.
-              </li>
-              <li>
-                Rem repellat cupiditate, nulla illo libero consectetur autem fugit laboriosam officia. Doloremque iusto quos laborum.
-              </li>
-              <li>
-                Distinctio assumenda aliquid dicta libero, iste nam velit nihil autem perspiciatis enim? Aperiam ratione nobis tenetur.
-              </li>
-            </ul>
+            <BlocksRenderer content={description} />
           </DescriptionProject>
           <h3>Detalles adicionales</h3>
           <DetailsProject>
             <li>
-              <p><span>Cliente:</span> TaskUnity</p>
+              <p><span>Cliente:</span> {client}</p>
             </li>
-            <li>
+            {/* <li>
               <p><span>Técnologías:</span> Desarrollador Web</p>
-            </li>
+            </li> */}
           </DetailsProject>
           <h3>Enlaces</h3>
           <LinksProject>
-            <a href='#' target='_blank'>Ver sitio</a>
-            |
-            <a href='#' target='_blank'>Repositiorio en Github</a>
+            <a href={siteURL} target='_blank'>Ver sitio</a>
+            {
+              (repoURL) && <a href={repoURL} target='_blank'>Repositiorio en Github</a>
+            }
           </LinksProject>
         </ContentProject>
       </GridProject>
