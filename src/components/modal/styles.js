979c889211b41286aff7872126a6609985633a6e
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-04-20 20:12:56 -0600

    Updated: styles

--- src/components/modal/styles.js
+++ src/components/modal/styles.js
@@ -6,19 +6,11 @@ export const ContainerModal = styled.div`
   z-index: 1;
   background-color: rgb(33 37 41 / 0.95);
   inset: 0;
+  overflow: auto;
 `;
 
 export const ContentModal = styled.div`
   max-width: 100rem;
   width: 95%;
-  margin: 10rem auto 5rem auto;
-  @media (min-width: 768px) {
-    margin: 15rem auto 0 auto;
-  }
-`;
-
-export const ContentModalChildren = styled.div`
-  padding: 4rem;
-  background-color: ${({ theme }) => theme.colors.white};
-  border-radius: 1rem;
+  margin: 5rem auto 5rem auto;
 `;
