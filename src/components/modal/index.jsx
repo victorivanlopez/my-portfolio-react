a3ace31dd64bdb0c4c24163c5e8fbc9528357680
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-04-22 21:55:17 -0600

    Added: <IconContainerModal />

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,6 +1,8 @@
 import { useRef } from 'react';
 import { usePortfolioContext } from '../../hooks';
-import { ContainerModal, ContentModal } from './styles';
+import { ContainerModal, ContentModal, IconContainerModal } from './styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 
 export const Modal = ({ children }) => {
   const { hiddenModal } = usePortfolioContext();
@@ -18,6 +20,11 @@ export const Modal = ({ children }) => {
       onClick={onClickModal}
     >
       <ContentModal className='scale-up-center'>
+        <IconContainerModal
+          onClick={hiddenModal}
+        >
+          <FontAwesomeIcon icon={faX} size='lg' color='#575655' />
+        </IconContainerModal>
         {children}
       </ContentModal>
     </ContainerModal>
