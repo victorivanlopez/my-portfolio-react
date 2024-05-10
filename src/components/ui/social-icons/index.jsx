89206f534d5101c10eb7fc4512f546c318b7ac17
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-05-09 21:56:29 -0600

    Added: social url

--- src/components/ui/social-icons/index.jsx
+++ src/components/ui/social-icons/index.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export const SocialIcons = () => {
   return (
     <>
-      <a href="https://mx.linkedin.com/" target="_blank" rel='noreferrer'>
+      <a href="https://www.linkedin.com/in/victorivanlopezg" target="_blank" rel='noreferrer'>
         <FontAwesomeIcon icon={faLinkedin} size='xl' />
       </a>
       <a href="https://github.com/victorivanlopez" target="_blank" rel='noreferrer'>
