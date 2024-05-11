dd9f76c1ec4a699492ae61efd748e429847fe60d
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-05-10 22:00:27 -0600

    Changed: texts

--- src/components/sections/about/AboutSection.jsx
+++ src/components/sections/about/AboutSection.jsx
@@ -8,7 +8,7 @@ export const AboutSection = () => {
 
       <AboutContent>
         <AboutInformation>
-          <p>Ingeniero en Telemática graduado de la Universidad de Colima, con sólida experiencia en el desarrollo de aplicaciones web centrándome en el ámbito del <span>Front-end y WordPress.</span></p>
+          <p>Ingeniero en Telemática graduado de la Universidad de Colima, con sólida experiencia en el desarrollo de aplicaciones web centrándome en el ámbito del <span>Front-End.</span></p>
           <p>Mi experiencia abarca el dominio de tecnologías del ecosistema de JavaScript principalmente con la librería de <span>React</span>, así como una habilidad probada en la implementación de soluciones utilizando <span>WordPress para proyectos con clientes.</span></p>
           <p>Destaco por mi capacidad para establecer una <span>buena comunicación con mis clientes</span>, lo que me ha permitido comprender a fondo las necesidades para sus proyectos.</p>
           <p>Motivado por el <span>aprendizaje continuo y mejora constante</span>, estoy ansioso por aportar mi experiencia y pasión por la tecnología en futuros proyectos.</p>
